491ab8c7a9c71e78ddefeece184cf67eecfbef1f
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-08-12 15:08:10 +0600

    3. print the pattern.c
    Simple input: 5
    
    Expected output: 
    
         *
        **
       ***
      ****
     *****

---
+++ 3. print the pattern.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int rows;
+    printf("Enter the number: ");
+    scanf("%d", &rows);
+
+    for(int i=1; i<=rows; i++){
+
+        for(int j=i; j<=rows; j++){
+            printf(" ");
+        }
+        for(int j=1; j<=i; j++){
+            printf("*");
+        }
+    printf("\n");
+    }
+
+    return 0;
+}
