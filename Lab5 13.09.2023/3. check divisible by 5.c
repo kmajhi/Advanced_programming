30cb7b0d558fbf3bad587f3fc3d427a324e03928
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-09-15 22:12:22 +0600

    3. check divisible by 5.c

---
+++ Lab5 13.09.2023/3. check divisible by 5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int CheckThisNumber(int function_variable) {
+    if (function_variable % 5 == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int main() {
+    int input_num;
+
+    printf("Enter a number: ");
+    scanf("%d", &input_num);
+
+    int functionErnewVariable = CheckThisNumber(input_num);
+
+    if (functionErnewVariable == 1) {
+        printf("The number is divisible by 5.\n");
+    } else {
+        printf("The number is not divisible by 5.\n");
+    }
+
+    return 0;
+}
+
+
