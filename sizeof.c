4b0cc73678fa2386287c8a67cdad99ff1e31119a
MD. SUJON MAHAMUD <126461844+kmajhi@users.noreply.github.com>
2023-10-27 23:54:13 +0600

    sizeof.c

--- sizeof.c
+++ sizeof.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main(){
-  int x []={1,2,3,4,5};
+    int x []={1,2,3,4,5,6,7,8,9,10};
+    int length = sizeof(x)/sizeof(x[0]);
 
-  printf("Size of the array is = %d", sizeof(x)/sizeof(x[0]));
-  return 0;
+    printf("Size of the array is = %d", length);
+    return 0;
 }
